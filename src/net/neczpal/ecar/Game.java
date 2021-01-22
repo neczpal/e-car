ec25520506c3d2b454db6d0e6bd6ead5b0db2e7f
Neczpal Abel <neczpal@gmail.com>
2021-01-22 15:20:38 +0100

    some warnings fixed

--- src/net/neczpal/ecar/Game.java
+++ src/net/neczpal/ecar/Game.java
@@ -1,6 +1,7 @@
 package net.neczpal.ecar;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.lwjgl.input.Keyboard;
 
@@ -10,13 +11,11 @@ import org.lwjgl.input.Keyboard;
 public class Game extends Thread implements Panel {
 
     private int car;//0/1/2
-    private ArrayList<int[]> oils;
-    private ArrayList<Integer> lines;
+    private final List<int[]> oils;
+    private final List<Integer> lines;
     private boolean stopped;
-    private int speed = 200;
     private int level;
     private final int[] levels = new int[]{1, 2, 3, 4, 5, 6, 7, 10, 20, 35, 67, 100, 166, 250, 400};
-    private int next_level;
 
     public Game() {
 
@@ -31,15 +30,15 @@ public class Game extends Thread implements Panel {
     public void run() {
 
         stopped = false;
-        speed = 16;
-        next_level = 0;
+        int speed = 16;
+        int next_level = 0;
         level = 0;
         lines.add(100);
         lines.add(300);
         lines.add(500);
         while (!stopped) {
             try {
-                //SAVOK
+                //Lines
                 for (int i = 0; i < lines.size(); i++) {
                     lines.set(i, lines.get(i) - 2);
                     if (lines.get(i) < -100) {
@@ -56,7 +55,7 @@ public class Game extends Thread implements Panel {
                 }
                 //OILS
                 for (int i = 0; i < oils.size(); i++) {
-                    //ütközik e a carral -> main menu
+                    //collide with car -> main menu
                     if (oils.get(i)[0] == car && 30 <= oils.get(i)[1] && oils.get(i)[1] <= 110) {
                         stopGame();
                         Window.window.changePanel(new MainMenu(level));
@@ -73,7 +72,7 @@ public class Game extends Thread implements Panel {
 
                 sleep(Math.max(speed, 1));
             } catch (InterruptedException ex) {
-
+                ex.printStackTrace();
             }
         }
     }
@@ -99,12 +98,12 @@ public class Game extends Thread implements Panel {
     public void draw() {
         Window.drawRect(0, 0, 350, 500, Window.road);
         Window.drawString(160, 470, Integer.toString(level), 16);
-        for (int i = 0; i < lines.size(); i++) {
-            Window.drawRect(110, lines.get(i), 10, 100, Window.line);
-            Window.drawRect(225, lines.get(i), 10, 100, Window.line);
+        for (Integer line : lines) {
+            Window.drawRect(110, line, 10, 100, Window.line);
+            Window.drawRect(225, line, 10, 100, Window.line);
         }
-        for (int i = 0; i < oils.size(); i++) {
-            Window.drawRect(20 + oils.get(i)[0] * 120, oils.get(i)[1] + 25, 50, 60, Window.oil);
+        for (int[] oil : oils) {
+            Window.drawRect(20 + oil[0] * 120, oil[1] + 25, 50, 60, Window.oil);
         }
         Window.drawRect(15 + car * 115, 10, 90, 150, Window.car);
     }
